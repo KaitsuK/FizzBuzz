33300d0ac2e0012930dd6e427bf4c2c5ad029b42
Kai Klemola <kai.klemola@icloud.com>
2018-10-16 19:33:20 +0300

    Edited user response texts

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,11 +5,13 @@ object Main extends App {
   if (args.length != 0) {
     var results :ArrayBuffer[Int] = new ArrayBuffer[Int]
     args.foreach(a => results += a.toInt)
-    println(FizzBuzzCalculator.getFizzBuzz(results.toSeq))
+    println("Fizz Buzz results for given values: " + (FizzBuzzCalculator.getFizzBuzz(results.toSeq)).toString())
+    //println(FizzBuzzCalculator.getFizzBuzz(results.toSeq))
 
   } else {
     val values = Seq(-2, -1, 0, 1, 2, 3, 99, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17)
-    println (FizzBuzzCalculator.getFizzBuzz(values))
+    println("Fizz Buzz example results for these default values: " + values.toString())
+    println("Results: " + (FizzBuzzCalculator.getFizzBuzz(values)).toString())
 
   }
 
